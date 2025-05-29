ee5968a7a651f04e68dfe96810594e3f35d99e94
Juan Agudelo <jagudelo@growthaccelerationpartners.com>
2025-05-28 21:51:57 -0500

    Add additional changes to the retry policy when is a formurlencoded POST

--- Application/EdFi.AdminConsole.HealthCheckService/Infrastructure/AppHttpClient.cs
+++ Application/EdFi.AdminConsole.HealthCheckService/Infrastructure/AppHttpClient.cs
@@ -31,7 +31,7 @@ public class AppHttpClient(HttpClient httpClient, ILogger logger, IOptions<AppSe
 
     public async Task<ApiResponse> SendAsync(string uriString, HttpMethod method, StringContent? content, AuthenticationHeaderValue? authenticationHeaderValue)
     {
-        var getByIdDelay = Backoff.ExponentialBackoff(
+        var delay = Backoff.ExponentialBackoff(
         TimeSpan.FromMilliseconds(Constants.RetryStartingDelayMilliseconds),
         _options.MaxRetryAttempts);
 
@@ -40,11 +40,11 @@ public class AppHttpClient(HttpClient httpClient, ILogger logger, IOptions<AppSe
         var retryPolicy = Policy
             .HandleResult<HttpResponseMessage>(r => r.StatusCode.IsPotentiallyTransientFailure())
             .WaitAndRetryAsync(
-                getByIdDelay,
+                delay,
                 (result, ts, retryAttempt, ctx) =>
                 {
-                    _logger.LogWarning("Retrying GET for resource '{UriString}'. Failed with status '{StatusCode}'. Retrying... (retry #{RetryAttempt} of {MaxRetryAttempts} with {TotalSeconds:N1}s delay)",
-                        uriString, result.Result.StatusCode, retryAttempt, _options.MaxRetryAttempts, ts.TotalSeconds);
+                    _logger.LogWarning("Retrying {HttpMethod} for resource '{UriString}'. Failed with status '{StatusCode}'. Retrying... (retry #{RetryAttempt} of {MaxRetryAttempts} with {TotalSeconds:N1}s delay)",
+                        method, uriString, result.Result.StatusCode, retryAttempt, _options.MaxRetryAttempts, ts.TotalSeconds);
                 });
 
         var response = await retryPolicy.ExecuteAsync(
@@ -54,8 +54,8 @@ public class AppHttpClient(HttpClient httpClient, ILogger logger, IOptions<AppSe
 
                 if (attempts > 1)
                 {
-                    _logger.LogDebug("GET for resource '{UriString}'. Attempt #{GetByIdAttempts}.",
-                        uriString, attempts);
+                    _logger.LogDebug("{HttpMethod} for resource '{UriString}'. Attempt #{Attempts}.",
+                        method, uriString, attempts);
                 }
 
                 var requestMessage = new HttpRequestMessage(method, uriString)
@@ -77,7 +77,7 @@ public class AppHttpClient(HttpClient httpClient, ILogger logger, IOptions<AppSe
 
         if (response.StatusCode != HttpStatusCode.OK)
         {
-            var message = $"GET request for '{uriString}' reference failed with status '{response.StatusCode}': {responseContent}";
+            var message = $"{method} request for '{uriString}' reference failed with status '{response.StatusCode}': {responseContent}";
             _logger.LogWarning(message);
         }
 
@@ -87,6 +87,22 @@ public class AppHttpClient(HttpClient httpClient, ILogger logger, IOptions<AppSe
     /// Access Token
     public async Task<ApiResponse> SendAsync(string uriString, HttpMethod method, FormUrlEncodedContent content, AuthenticationHeaderValue? authenticationHeaderValue)
     {
+        var delay = Backoff.ExponentialBackoff(
+        TimeSpan.FromMilliseconds(Constants.RetryStartingDelayMilliseconds),
+        _options.MaxRetryAttempts);
+
+        int attempts = 0;
+
+        var retryPolicy = Policy
+            .HandleResult<HttpResponseMessage>(r => r.StatusCode.IsPotentiallyTransientFailure())
+            .WaitAndRetryAsync(
+                delay,
+                (result, ts, retryAttempt, ctx) =>
+                {
+                    _logger.LogWarning("Retrying {HttpMethod} for resource '{UriString}'. Failed with status '{StatusCode}'. Retrying... (retry #{RetryAttempt} of {MaxRetryAttempts} with {TotalSeconds:N1}s delay)",
+                        method, uriString, result.Result.StatusCode, retryAttempt, _options.MaxRetryAttempts, ts.TotalSeconds);
+                });
+
         using var requestMessage = new HttpRequestMessage(method, uriString)
         {
             Content = content
@@ -97,7 +113,32 @@ public class AppHttpClient(HttpClient httpClient, ILogger logger, IOptions<AppSe
             _httpClient.DefaultRequestHeaders.Authorization = authenticationHeaderValue;
         }
 
-        var response = await _httpClient.SendAsync(requestMessage);
+        var response = await retryPolicy.ExecuteAsync(
+            async (ctx, ct) =>
+            {
+                attempts++;
+
+                if (attempts > 1)
+                {
+                    _logger.LogDebug("{HttpMethod} for resource '{UriString}'. Attempt #{Attempts}.",
+                        method, uriString, attempts);
+                }
+
+                var requestMessage = new HttpRequestMessage(method, uriString)
+                {
+                    Content = content
+                };
+
+                if (authenticationHeaderValue != null)
+                {
+                    requestMessage.Headers.Authorization = authenticationHeaderValue;
+                }
+
+                return await _httpClient.SendAsync(requestMessage, ct);
+            },
+            [],
+            CancellationToken.None);
+
         var responseContent = await response.Content.ReadAsStringAsync();
         return new ApiResponse(response.StatusCode, responseContent, response.Headers);
     }
